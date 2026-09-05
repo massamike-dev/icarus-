8a3ad2a86515bd2d6ed07bef95516126191a25a2
massamike-dev <itsmike4you@gmail.com>
2026-09-05 14:15:10 -0700

    Configure GitHub Maven repository in settings.gradle.kts
    Added GitHub Maven repository with authentication using GITHUB_TOKEN.

--- native/android/settings.gradle.kts
+++ native/android/settings.gradle.kts
@@ -1,3 +1,13 @@
+import java.util.Properties
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
+val localProperties = Properties().apply {
+    val propertiesFile = file("local.properties")
+    if (propertiesFile.exists()) {
+        propertiesFile.inputStream().use(::load)
+    }
+}
+
 pluginManagement {
     repositories {
         google()
@@ -8,9 +18,20 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
+
+        maven {
+            url = uri("https://maven.pkg.github.com/facebook/meta-wearables-dat-android")
+            credentials {
+                username = "x-access-token"
+                password =
+                    providers.environmentVariable("GITHUB_TOKEN").orNull
+                        ?: localProperties.getProperty("github_token")
+            }
+        }
     }
 }
 
