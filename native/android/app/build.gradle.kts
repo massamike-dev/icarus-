e960e134af67cbac3ac55016f27ccec1dcb9abe4
massamike-dev <itsmike4you@gmail.com>
2026-09-08 21:33:55 -0700

    Link production Android app to XREAL module

--- native/android/app/build.gradle.kts
+++ native/android/app/build.gradle.kts
@@ -69,6 +69,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":xreal"))
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.10.0")
