960dc3e581a9f0bfcc18d8d5f8a4d721ff3a2e64
massamike-dev <itsmike4you@gmail.com>
2026-09-08 21:30:54 -0700

    Link Android app to XREAL glasses module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":xreal"))
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
